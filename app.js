bf085e903c310223ca4fa7e532415c809fe1f3e7
26Pushkal <119613432+26Pushkal@users.noreply.github.com>
2025-02-25 17:29:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-/// Initialize express app
+// Initialize express app
 const app = express();
 const port = process.env.PORT || 3000;
 
